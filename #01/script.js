1992ab510cfd6cc832bc22f8c272a9fcdfcef7b0
Frank7Dev <frank.code31@gmail.com>
2021-12-03 21:34:27 -0500

    RETO 1° / V 0.2

--- #01/script.js
+++ #01/script.js
@@ -1,17 +1,18 @@
+// 1. Array de ovejas
 const ovejas = [
   { name: 'Noa', color: 'azul' },
   { name: 'Euge', color: 'rojo' },
   { name: 'Navidad', color: 'rojo' },
   { name: 'Ki Na Ma', color: 'rojo'}
-]
+];
 
+// 2. Funcion que filtra las ovejas rojas y que en su nombre tengan la letra "n" y "a"
 function contarOvejas(ovejas) {
   return ovejas
     .filter( ({color}) => color === "rojo" )
-    .filter( ({name}) => name.toUpperCase().includes("N"))
-    .filter( ({name}) => name.toUpperCase().includes("A"))
+    .filter( ({name}) => /n/gi.test(name) && /a/gi.test(name) );
 }
 
-const ovejasFiltradas = contarOvejas(ovejas)
-
-console.log(ovejasFiltradas)
+// 3. Ejecutamos la funcion y mostramos en consola el resultado
+const ovejasFiltradas = contarOvejas(ovejas);
+console.log(ovejasFiltradas);
